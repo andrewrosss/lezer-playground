942f6f21644b322dd2e3dc638f06e40eb3b1a481
Andrew Ross <andrew.ross.mail@gmail.com>
2023-03-29 01:51:25 -0400

    Update '/' layout using <ResizableSpliteContainer />

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,39 @@
+import { ResizableSplitContainer } from "~/components/resizable-split-container";
+
 export default function Home() {
   return (
-    <main class="h-full w-full">
-      <h1>Hello world!</h1>
-    </main>
+    <ResizableSplitContainer
+      style={{ "grid-area": "main" }}
+      splitDirection="horizontal"
+      first={<_GrammarContainer />}
+      second={
+        <ResizableSplitContainer
+          splitDirection="vertical"
+          first={<_SpecimenContainer />}
+          second={<_TreeContainer />}
+        />
+      }
+    />
   );
 }
+
+const _GrammarContainer = () => (
+  <section class="w-full h-full flex flex-col items-stretch">
+    <h2 class="px-4 py-2 text-sm border-b border-slate-800">Grammar</h2>
+    <div class="grow bg-neutral-800" />
+  </section>
+);
+
+const _SpecimenContainer = () => (
+  <section class="w-full h-full flex flex-col items-stretch">
+    <h2 class="px-4 py-2 text-sm border-b border-slate-800">Specimen</h2>
+    <div class="grow bg-neutral-800" />
+  </section>
+);
+
+const _TreeContainer = () => (
+  <section class="w-full h-full flex flex-col items-stretch">
+    <h2 class="px-4 py-2 text-sm border-b border-slate-800">Tree</h2>
+    <div class="grow bg-neutral-800" />
+  </section>
+);
