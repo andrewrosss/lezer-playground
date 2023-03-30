5556f67de29ee92fdb15e607bbe49aa7db65cc14
Andrew Ross <andrew.ross.mail@gmail.com>
2023-03-30 01:20:44 -0400

    Update '/' to include codemirror editors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Editor } from "~/components/editor";
 import { ResizableSplitContainer } from "~/components/resizable-split-container";
 
 export default function Home() {
@@ -20,20 +21,24 @@ export default function Home() {
 const _GrammarContainer = () => (
   <section class="w-full h-full flex flex-col items-stretch">
     <h2 class="px-4 py-2 text-sm border-b border-slate-800">Grammar</h2>
-    <div class="grow bg-neutral-800" />
+
+    {/* the classes: 'flex-auto overflow-y-auto h-24' are inspired by this
+        answer (https://stackoverflow.com/a/14964944) to make the element
+        grow and scroll when it's bigger */}
+    <Editor class="text-sm flex-auto overflow-y-auto h-24" />
   </section>
 );
 
 const _SpecimenContainer = () => (
   <section class="w-full h-full flex flex-col items-stretch">
     <h2 class="px-4 py-2 text-sm border-b border-slate-800">Specimen</h2>
-    <div class="grow bg-neutral-800" />
+    <Editor class="text-sm flex-auto overflow-y-auto h-24" />
   </section>
 );
 
 const _TreeContainer = () => (
   <section class="w-full h-full flex flex-col items-stretch">
     <h2 class="px-4 py-2 text-sm border-b border-slate-800">Tree</h2>
-    <div class="grow bg-neutral-800" />
+    <Editor class="text-sm flex-auto overflow-y-auto h-24" />
   </section>
 );
