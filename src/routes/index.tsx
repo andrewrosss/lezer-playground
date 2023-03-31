be67097dad8a7a258f52b280edf27e05a8b406e3
Andrew Ross <andrew.ross.mail@gmail.com>
2023-03-31 00:21:37 -0400

    Update '/' - fix overflow behaviour of editors

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -19,26 +19,43 @@ export default function Home() {
 }
 
 const _GrammarContainer = () => (
-  <section class="w-full h-full flex flex-col items-stretch">
-    <h2 class="px-4 py-2 text-sm border-b border-slate-800">Grammar</h2>
+  <section
+    class="w-full h-full grid"
+    style={{ "grid-template-rows": "auto 1fr" }}
+  >
+    <h2 class="px-4 py-2 text-sm border-b border-slate-700">Grammar</h2>
 
     {/* the classes: 'flex-auto overflow-y-auto h-24' are inspired by this
         answer (https://stackoverflow.com/a/14964944) to make the element
         grow and scroll when it's bigger */}
-    <Editor class="text-sm flex-auto overflow-y-auto h-24" />
+    <div class="overflow-auto flex flex-col">
+      <Editor class="flex-auto overflow-y-auto h-24 text-sm" />
+    </div>
   </section>
 );
 
 const _SpecimenContainer = () => (
-  <section class="w-full h-full flex flex-col items-stretch">
-    <h2 class="px-4 py-2 text-sm border-b border-slate-800">Specimen</h2>
-    <Editor class="text-sm flex-auto overflow-y-auto h-24" />
+  <section
+    class="w-full h-full grid"
+    style={{ "grid-template-rows": "auto 1fr" }}
+  >
+    <h2 class="px-4 py-2 text-sm border-b border-slate-700">Specimen</h2>
+
+    <div class="overflow-auto flex flex-col">
+      <Editor class="flex-auto overflow-y-auto h-24 text-sm" />
+    </div>
   </section>
 );
 
 const _TreeContainer = () => (
-  <section class="w-full h-full flex flex-col items-stretch">
-    <h2 class="px-4 py-2 text-sm border-b border-slate-800">Tree</h2>
-    <Editor class="text-sm flex-auto overflow-y-auto h-24" />
+  <section
+    class="w-full h-full grid"
+    style={{ "grid-template-rows": "auto 1fr" }}
+  >
+    <h2 class="px-4 py-2 text-sm border-b border-slate-700">Tree</h2>
+
+    <div class="overflow-auto flex flex-col">
+      <Editor class="flex-auto overflow-y-auto h-24 text-sm" />
+    </div>
   </section>
 );
