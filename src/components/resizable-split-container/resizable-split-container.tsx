7bd32a93a9b043275ccd4f6a02f73ac40a4c2b57
Andrew Ross <andrew.ross.mail@gmail.com>
2023-03-31 00:23:12 -0400

    Update resizable-split-container
    Remove 3 dots and clean up flex-basis computation (onMouseMove)

--- src/components/resizable-split-container/resizable-split-container.tsx
+++ src/components/resizable-split-container/resizable-split-container.tsx
@@ -9,9 +9,9 @@ type Props = ComponentProps<"div"> & {
 };
 
 export const ResizableSplitContainer = (props: Props) => {
-  let containerRef: HTMLDivElement;
-  let firstRef: HTMLDivElement;
-  let secondRef: HTMLDivElement;
+  let containerRef!: HTMLDivElement;
+  let firstRef!: HTMLDivElement;
+  let secondRef!: HTMLDivElement;
   const [local, rest] = splitProps(props, [
     "splitDirection",
     "first",
@@ -32,42 +32,24 @@ export const ResizableSplitContainer = (props: Props) => {
 
   const handleMouseMove = () => {
     if (isPressed()) {
-      if (local.splitDirection === "vertical") {
-        // minimum height of 200px for each panel when veritcal
-        if (pos().y < 200) return;
-        if (pos().y > window.innerHeight - 200) return;
-
-        const rect = containerRef.getBoundingClientRect();
-        console.log(rect);
-        // determine percent of container above the mouse
-        const percent = (pos().y - rect.top) / rect.height;
-        // set the flex-basis of the first panel to that percent
-        const basis = `calc(${percent * 100}% - 0.5rem)`; // 0.5rem for the divider
-        firstRef.style.setProperty("flex-basis", basis);
-        // set the flex-basis of the second panel to the remaining percent
-        const secondBasis = `calc(${(1 - percent) * 100}% - 0.5rem)`; // 0.5rem for the divider
-        secondRef.style.setProperty("flex-basis", secondBasis);
-      } else {
-        // minimum width of 300px for each panel when horizontal
-        if (pos().x < 300) return;
-        if (pos().x > window.innerWidth - 300) return;
-
-        const rect = containerRef.getBoundingClientRect();
-        // determine percent of container to the left of the mouse
-        const percent = (pos().x - rect.left) / rect.width;
-        // set the flex-basis of the first panel to that percent
-        const basis = `calc(${percent * 100}% - 0.5rem)`; // 0.5rem for the divider
-        firstRef.style.setProperty("flex-basis", basis);
-        // set the flex-basis of the second panel to the remaining percent
-        const secondBasis = `calc(${(1 - percent) * 100}% - 0.5rem)`; // 0.5rem for the divider
-        secondRef.style.setProperty("flex-basis", secondBasis);
-      }
+      const rect = containerRef.getBoundingClientRect();
+      // determine percent of container above (vertical) or
+      // to the left (horizontal) of the mouse
+      const percent =
+        local.splitDirection === "vertical"
+          ? (pos().y - rect.top) / rect.height
+          : (pos().x - rect.left) / rect.width;
+      // set the flex-basis of the first panel to that percent
+      const basis = `calc(${percent * 100}% - 3px)`; // 3px for the divider
+      firstRef.style.setProperty("flex-basis", basis);
+      // set the flex-basis of the second panel to the remaining percent
+      const secondBasis = `calc(${(1 - percent) * 100}% - 3px)`; // 3px for the divider
+      secondRef.style.setProperty("flex-basis", secondBasis);
     }
   };
 
   return (
     <div
-      // @ts-ignore
       ref={containerRef}
       class="w-full h-full flex items-stretch"
       classList={{
@@ -78,47 +60,21 @@ export const ResizableSplitContainer = (props: Props) => {
       {...rest}
       onMouseMove={handleMouseMove}
     >
-      {/* @ts-ignore */}
-      <div ref={firstRef} class="grow shrink-0 basis-[calc(50%-0.5rem)]">
+      <div ref={firstRef} class="grow shrink-0 basis-[calc(50%-3px)]">
         {local.first}
       </div>
 
       <div
-        class="grow-0 shrink-0 flex justify-center  border-slate-800 hover:bg-slate-700/75"
+        class="grow-0 shrink-0 flex justify-center  bg-slate-700"
         classList={{
-          "flex-row cursor-row-resize border-y-2":
-            local.splitDirection === "vertical",
-          "flex-col cursor-col-resize border-x-2":
-            local.splitDirection === "horizontal",
+          "cursor-row-resize h-[6px]": local.splitDirection === "vertical",
+          "cursor-col-resize w-[6px]": local.splitDirection === "horizontal",
         }}
         onMouseDown={handleMouseDown}
         onMouseUp={handleMouseUp}
-      >
-        <span
-          class="w-1 h-1 rounded-full bg-slate-300"
-          classList={{
-            "my-0.5 mx-1": local.splitDirection === "vertical",
-            "my-1 mx-0.5": local.splitDirection === "horizontal",
-          }}
-        ></span>
-        <span
-          class="w-1 h-1 rounded-full bg-slate-300"
-          classList={{
-            "my-0.5 mx-1": local.splitDirection === "vertical",
-            "my-1 mx-0.5": local.splitDirection === "horizontal",
-          }}
-        ></span>
-        <span
-          class="w-1 h-1 rounded-full bg-slate-300"
-          classList={{
-            "my-0.5 mx-1": local.splitDirection === "vertical",
-            "my-1 mx-0.5": local.splitDirection === "horizontal",
-          }}
-        ></span>
-      </div>
+      />
 
-      {/* @ts-ignore */}
-      <div ref={secondRef} class="grow shrink-0 basis-[calc(50%-0.5rem)]">
+      <div ref={secondRef} class="grow shrink-0 basis-[calc(50%-3px)]">
         {local.second}
       </div>
     </div>
