eda609c45bfdd27da12fccd2a63273063f0564c1
Andrew Ross <andrew.ross.mail@gmail.com>
2023-04-02 00:11:14 -0400

    Update '/' - use new wrapped editor components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,7 @@
-import { Editor } from "~/components/editor";
+import { GrammarEditor } from "~/components/grammar-editor";
 import { SolidSplit } from "~/components/solid-split";
+import { SpecimenEditor } from "~/components/specimen-editor";
+import { TreeEditor } from "~/components/tree-editor";
 
 export default function Home() {
   return (
@@ -8,59 +10,17 @@ export default function Home() {
       options={{ direction: "horizontal", gutterSize: 8, minSize: 100 }}
       gutter={gutterFn}
     >
-      <_GrammarContainer />
+      <GrammarEditor />
       <SolidSplit
         options={{ direction: "vertical", gutterSize: 8, minSize: 100 }}
         gutter={gutterFn}
       >
-        <_SpecimenContainer />
-        <_TreeContainer />
+        <SpecimenEditor />
+        <TreeEditor />
       </SolidSplit>
     </SolidSplit>
   );
 }
-
-const _GrammarContainer = () => (
-  <section
-    class="w-full h-full grid"
-    style={{ "grid-template-rows": "auto 1fr" }}
-  >
-    <h2 class="px-4 py-2 text-sm border-b border-slate-700">Grammar</h2>
-
-    {/* the classes: 'flex-auto overflow-y-auto h-24' are inspired by this
-        answer (https://stackoverflow.com/a/14964944) to make the element
-        grow and scroll when it's bigger */}
-    <div class="overflow-auto flex flex-col">
-      <Editor class="flex-auto overflow-y-auto h-24 text-sm" />
-    </div>
-  </section>
-);
-
-const _SpecimenContainer = () => (
-  <section
-    class="w-full h-full grid"
-    style={{ "grid-template-rows": "auto 1fr" }}
-  >
-    <h2 class="px-4 py-2 text-sm border-b border-slate-700">Specimen</h2>
-
-    <div class="overflow-auto flex flex-col">
-      <Editor class="flex-auto overflow-y-auto h-24 text-sm" />
-    </div>
-  </section>
-);
-
-const _TreeContainer = () => (
-  <section
-    class="w-full h-full grid"
-    style={{ "grid-template-rows": "auto 1fr" }}
-  >
-    <h2 class="px-4 py-2 text-sm border-b border-slate-700">Tree</h2>
-
-    <div class="overflow-auto flex flex-col">
-      <Editor class="flex-auto overflow-y-auto h-24 text-sm" />
-    </div>
-  </section>
-);
 
 const gutterFn = (_: number, direction: "horizontal" | "vertical") =>
   (direction === "vertical" ? (
