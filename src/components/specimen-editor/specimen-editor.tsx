d5c7d3e0a17703c030b854eec591df53c0e2fc92
Andrew Ross <andrew.ross.mail@gmail.com>
2023-04-07 01:13:57 -0400

    Update specimen-editor heading to 'Test String'

--- src/components/specimen-editor/specimen-editor.tsx
+++ src/components/specimen-editor/specimen-editor.tsx
@@ -17,7 +17,7 @@ export const SpecimenEditor = () => {
       class="w-full h-full grid"
       style={{ "grid-template-rows": "auto 1fr" }}
     >
-      <h2 class="px-4 py-2 text-sm border-b border-slate-700">Specimen</h2>
+      <h2 class="px-4 py-2 text-sm border-b border-slate-700">Test String</h2>
 
       {/* the classes: 'flex-auto overflow-y-auto h-24' are inspired by this
           answer (https://stackoverflow.com/a/14964944) to make the element
